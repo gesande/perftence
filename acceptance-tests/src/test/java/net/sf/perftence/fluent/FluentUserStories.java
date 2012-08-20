973d88bf6921ef8bbc0914bdc58639571671aede
gsande <geir.sande@luukku.com>
2012-08-20 06:01:12 +0000

    provided test example for fluent based tests

---
+++ acceptance-tests/src/test/java/net/sf/perftence/fluent/FluentUserStories.java
@@ -0,0 +1,138 @@
+package net.sf.perftence.fluent;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import junit.framework.Assert;
+import net.sf.perftence.AbstractMultiThreadedTest;
+import net.sf.perftence.DefaultTestRunner;
+import net.sf.perftence.Executable;
+import net.sf.perftence.reporting.Duration;
+import net.sf.perftence.reporting.summary.Summary;
+import net.sf.perftence.reporting.summary.SummaryAppender;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(DefaultTestRunner.class)
+public class FluentUserStories extends AbstractMultiThreadedTest {
+    private static final Logger LOG = LoggerFactory
+            .getLogger(FluentUserStories.class);
+    private Executable executor;
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
+
+    private int i;
+
+    private final Executable executor() {
+        return this.executor;
+    }
+
+    @Before
+    public void before() throws Exception {
+        this.i = 0;
+        this.executor = new Executable() {
+            @Override
+            public void execute() {
+                FluentUserStories.this.i++;
+                if (FluentUserStories.this.i % 1000 == 0) {
+                    log().info("Executing...{}", FluentUserStories.this.i);
+                }
+            }
+        };
+    }
+
+    @SuppressWarnings("static-method")
+    @Test
+    public void learning() {
+        final AtomicInteger i = new AtomicInteger(0);
+        for (int index = 0; index < 100; index++) {
+            log().info("{} % 10 == 0 -> {}", i.intValue(),
+                    i.intValue() % 10 == 0);
+            i.incrementAndGet();
+        }
+
+    }
+
+    @SuppressWarnings("static-method")
+    @After
+    public void after() {
+        log().info("Its over...finally...phuuiiii, i'm done.");
+    }
+
+    private static Logger log() {
+        return LOG;
+    }
+
+    @Test
+    public void invocationCount() throws Exception {
+        final AtomicInteger i = new AtomicInteger(0);
+        test().setup(setup().threads(3).invocations(10).build())
+                .executable(new Executable() {
+                    @Override
+                    public void execute() throws Exception {
+                        executor().execute();
+                        i.incrementAndGet();
+                    }
+                }).start();
+        Assert.assertEquals("Invocation count doesn't match!", 10, i.get());
+    }
+
+    @Test
+    public void durationMultiThreadSleepingMoreThanOneSecondExecutable()
+            throws Exception {
+        test().setup(
+                setup().duration(Duration.seconds(15)).threads(10)
+                        .invocationRange(1000).throughputRange(30).build())
+                .executable(new Executable() {
+
+                    @Override
+                    public void execute() throws Exception {
+                        Thread.sleep(RANDOM.nextInt(1000));
+                    }
+                }).start();
+    }
+
+    @Test
+    public void customSummaryAppender() throws Exception {
+        test().setup(
+                setup().duration(Duration.seconds(15)).threads(10)
+                        .invocationRange(1000).throughputRange(30)
+                        .summaryAppender(new SummaryAppender() {
+                            @Override
+                            public void append(Summary<?> summary) {
+                                summary.text("Here's something cool!")
+                                        .endOfLine()
+                                        .bold("And some bolded text")
+                                        .endOfLine();
+                            }
+                        }).build()).executable(new Executable() {
+
+            @Override
+            public void execute() throws Exception {
+                Thread.sleep(RANDOM.nextInt(1000));
+            }
+        }).start();
+    }
+
+    @Ignore
+    @Test
+    public void durationBasedTestWithWaitingMoreThanDuration() throws Exception {
+        test().setup(
+                setup().threads(100).duration(Duration.seconds(10)).build())
+                .executable(new Executable() {
+
+                    @Override
+                    public void execute() throws Exception {
+                        int wait = RANDOM.nextInt(100) + 1;
+                        log().debug("Wait time {}", wait);
+                        Thread.sleep(Duration.seconds(wait));
+                    }
+                }).start();
+    }
+
+}
---
+++ fluent-based-example/src/test/java/net/sf/perftence/FluentBasedTestExample.java
@@ -0,0 +1,197 @@
+package net.sf.perftence;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import net.sf.perftence.reporting.Duration;
+import net.sf.perftence.reporting.summary.Summary;
+import net.sf.perftence.reporting.summary.SummaryAppender;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Acts as an example of how a developer can define performance test fluently
+ * 
+ */
+@RunWith(DefaultTestRunner.class)
+public final class FluentBasedTestExample extends AbstractMultiThreadedTest {
+
+    private final static Logger LOG = LoggerFactory
+            .getLogger(FluentBasedTestExample.class);
+    private final static Random RANDOM = new Random(System.currentTimeMillis());
+
+    @Test
+    public void invocationBased() throws Exception {
+        test().setup(setup().invocations(100).invocationRange(10).build())
+                .executable(new Executable() {
+                    @Override
+                    public void execute() throws Exception {
+                        sleep(10);
+                    }
+                }).start();
+    }
+
+    @Test
+    public void threadBased() throws Exception {
+        test().setup(
+                setup().threads(100).invocations(5000).invocationRange(20)
+                        .build()).executable(new Executable() {
+            @Override
+            public void execute() throws Exception {
+                sleep(10);
+            }
+        }).start();
+    }
+
+    @Test
+    public void durationBasedSingleThread() throws Exception {
+        test().setup(setup().duration(Duration.seconds(5)).build())
+                .executable(new Executable() {
+                    @Override
+                    public void execute() throws Exception {
+                        sleep(100);
+                    }
+                }).start();
+    }
+
+    @Test
+    public void durationBasedMultiThread() throws Exception {
+        test().setup(
+                setup().duration(Duration.seconds(2)).threads(2)
+                        .invocationRange(20).throughputRange(2000000).build())
+                .executable(new Executable() {
+                    @Override
+                    public void execute() throws Exception {
+                        sleep(100);
+                    }
+                }).start();
+    }
+
+    @Test
+    public void performanceRequirements() throws Exception {
+        test().setup(setup().threads(10).invocations(100).build())
+                .requirements(
+                        requirements().average(30).max(200).median(20).build())
+                .executable(new Executable() {
+                    @Override
+                    public void execute() throws Exception {
+                        sleep(20);
+                    }
+                }).start();
+    }
+
+    @Test
+    public void durationWithRanges() throws Exception {
+        test().setup(
+                setup().duration(Duration.seconds(5)).threads(10)
+                        .invocationRange(100).throughputRange(500).build())
+                .executable(new Executable() {
+
+                    @Override
+                    public void execute() throws Exception {
+                        sleep(random(100) + 1);
+                    }
+
+                }).start();
+    }
+
+    @Test
+    public void customSummaryAppender() throws Exception {
+        final SummaryAppender summaryAppender = new SummaryAppender() {
+            @Override
+            public void append(Summary<?> summary) {
+                summary.text("Here's something cool!").endOfLine()
+                        .bold("And some bolded text").endOfLine();
+            }
+        };
+        test().setup(
+                setup().duration(Duration.seconds(15)).threads(10)
+                        .invocationRange(1000).throughputRange(30)
+                        .summaryAppender(summaryAppender).build())
+                .executable(new Executable() {
+
+                    @Override
+                    public void execute() throws Exception {
+                        randomSleep(1000);
+                    }
+
+                }).start();
+    }
+
+    @Test
+    public void noInvocationGraph() throws Exception {
+        test().noInvocationGraph()
+                .setup(setup().duration(Duration.seconds(5)).threads(10)
+                        .invocationRange(1000).throughputRange(30).build())
+                .executable(new Executable() {
+
+                    @Override
+                    public void execute() throws Exception {
+                        randomSleep(1000);
+                    }
+                }).start();
+    }
+
+    @Test
+    public void threadBasedWithAllowedException() throws Exception {
+        final ExecutorBehavingBadlyHalfTheTime failingExecutor = new ExecutorBehavingBadlyHalfTheTime();
+        test().setup(
+                setup().threads(10).invocations(100).throughputRange(50)
+                        .build()).allow(MyException.class)
+                .executable(new Executable() {
+                    @Override
+                    public void execute() throws Exception {
+                        failingExecutor.execute();
+                    }
+                }).start();
+    }
+
+    class ExecutorBehavingBadlyHalfTheTime implements Executable {
+        final AtomicInteger counter = new AtomicInteger();
+
+        @Override
+        public void execute() throws Exception {
+            if (this.counter.get() % 2 == 0) {
+                this.counter.incrementAndGet();
+                throw new MyException(
+                        "null pointer executing NullPointerExceptionExecutor");
+            }
+            this.counter.incrementAndGet();
+            Thread.sleep(500);
+        }
+
+    }
+
+    class MyException extends Exception {
+
+        public MyException(final String msg) {
+            super(msg);
+        }
+    }
+
+    private static int random(final int n) {
+        return RANDOM.nextInt(n);
+    }
+
+    private static void randomSleep(final int max) throws InterruptedException {
+        sleep(random(max));
+    }
+
+    private static void sleep(final int value) throws InterruptedException {
+        Thread.sleep(value);
+    }
+
+    private static Logger log() {
+        return LOG;
+    }
+
+    @SuppressWarnings("static-method")
+    @After
+    public void after() {
+        log().info("Its over...finally...phuuiiii, i'm done.");
+    }
+}
