68f5b8698c67c59419067609b32c95c928d176fc
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2012-08-21 08:16:53 +0000

    distribution: extracted some scripting to a library to simplify entry point scripts

--- trunk/build/distribution/for/fluent-based-example.sh
+++ trunk/build/distribution/for/fluent-based-example.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -eu
+HERE=$(dirname $0)
+. $HERE/lib.sh
 
-distribution-with-sources() {
-  local PROJECT=$1
-  gradle clean $PROJECT:build $PROJECT:dist $PROJECT:sourcesJar
-}
-
-make-dist-tar() {
-  local PROJECT=$1
-  cd $PROJECT/build/
-  rm -rf dist
-  mkdir dist
-  cp libs/*.jar dist
-  cp distributions/*.zip dist
-  tar -cf $PROJECT-dist.tar dist
-}
-
-distribution-with-sources "fluent-based-example"
-make-dist-tar "fluent-based-example"
+build-distribution-artifact "fluent-based-example"
--- trunk/build/distribution/for/perftence/with/sources.sh
+++ trunk/build/distribution/for/perftence/with/sources.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -eu
-gradle clean perftence:build perftence:dist perftence:sourcesJar
+set -eu
+HERE=$(dirname $0)
+. $HERE/../../lib.sh
+
+build-distribution-artifact "perftence"
 
