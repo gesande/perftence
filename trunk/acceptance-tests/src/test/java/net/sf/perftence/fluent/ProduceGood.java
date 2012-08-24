d150a7b36edb8c8feb81128158f91f8d92fe4c7f
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2012-08-24 11:57:24 +0000

    good graph results for agent-based tests

--- trunk/acceptance-tests/src/test/java/net/sf/perftence/fluent/ProduceGood.java
+++ trunk/acceptance-tests/src/test/java/net/sf/perftence/fluent/ProduceGood.java
@@ -1,11 +1,20 @@
 package net.sf.perftence.fluent;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 import java.util.Stack;
 
 import net.sf.perftence.AbstractMultiThreadedTest;
 import net.sf.perftence.DefaultTestRunner;
 import net.sf.perftence.Executable;
+import net.sf.perftence.agents.TestAgent;
+import net.sf.perftence.agents.TestTask;
+import net.sf.perftence.agents.TestTaskCategory;
+import net.sf.perftence.agents.TestTaskReporter;
+import net.sf.perftence.agents.Time;
+import net.sf.perftence.agents.TimeSpecificationFactory;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +23,7 @@ import org.junit.runner.RunWith;
 public class ProduceGood extends AbstractMultiThreadedTest {
 
     @Test
-    public void graphs() {
+    public void graphsForFluentBased() {
         final int invocations = 50000;
         final Stack<Integer> stack = new Stack<Integer>();
         setupStack(stack);
@@ -28,6 +37,59 @@ public class ProduceGood extends AbstractMultiThreadedTest {
                         Thread.sleep(stack.pop());
                     }
                 }).start();
+    }
+
+    @Test
+    public void graphsForAgentBased() {
+        final Stack<Integer> stack = new Stack<Integer>();
+        setupStack(stack);
+        agentBasedTest().latencyGraphForAll().agents(agents(50000, stack))
+                .workers(400).start();
+    }
+
+    enum Category implements TestTaskCategory {
+        Sleep;
+    }
+
+    private Collection<TestAgent> agents(final int size,
+            final Stack<Integer> stack) {
+        List<TestAgent> list = new ArrayList<TestAgent>();
+        for (int i = 0; i < size; i++) {
+            list.add(new TestAgent() {
+
+                @Override
+                public TestTask firstTask() {
+                    return newTask(stack);
+                }
+
+                private TestTask newTask(final Stack<Integer> stack) {
+                    return new TestTask() {
+
+                        @Override
+                        public Time when() {
+                            return TimeSpecificationFactory.now();
+                        }
+
+                        @Override
+                        public void run(final TestTaskReporter reporter)
+                                throws Exception {
+                            Thread.sleep(!stack.empty() ? stack.pop() : 30);
+                        }
+
+                        @Override
+                        public TestTask nextTaskIfAny() {
+                            return stack.empty() ? null : newTask(stack);
+                        }
+
+                        @Override
+                        public TestTaskCategory category() {
+                            return Category.Sleep;
+                        }
+                    };
+                }
+            });
+        }
+        return list;
     }
 
     private void setupStack(final Stack<Integer> stack) {
@@ -58,7 +120,7 @@ public class ProduceGood extends AbstractMultiThreadedTest {
         }
     }
 
-    private int random(final Random random, final int min, int max) {
+    private int random(final Random random, final int min, final int max) {
         final int i = random.nextInt(max);
         return i < min ? random(random, min, max) : i;
     }
