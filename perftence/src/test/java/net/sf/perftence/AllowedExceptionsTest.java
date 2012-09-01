f1235d42ff2bcda5cb0a0f874e11bb03923b28e5
gsande <geir.sande@luukku.com>
2012-09-01 16:25:40 +0000

    added new unit tests

---
+++ perftence/src/test/java/net/sf/perftence/AllowedExceptionsTest.java
@@ -0,0 +1,25 @@
+package net.sf.perftence;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AllowedExceptionsTest {
+
+    @SuppressWarnings("static-method")
+    @Test
+    public void test() {
+        AllowedExceptions ae = new AllowedExceptions();
+        MyException exception = new MyException();
+        assertFalse(ae.isAllowed(exception));
+        ae.allow(MyException.class);
+        assertTrue(ae.isAllowed(exception));
+    }
+
+    final static class MyException extends Exception {
+        public MyException() {
+            super();
+        }
+    }
+}
