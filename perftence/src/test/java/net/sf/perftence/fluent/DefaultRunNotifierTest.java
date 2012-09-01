e52ad5b756a4a6e271b8854d307540c1e11943af
gsande <geir.sande@luukku.com>
2012-09-01 16:09:40 +0000

    added new unit tests

---
+++ perftence/src/test/java/net/sf/perftence/fluent/DefaultRunNotifierTest.java
@@ -0,0 +1,19 @@
+package net.sf.perftence.fluent;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DefaultRunNotifierTest {
+
+    @SuppressWarnings("static-method")
+    @Test
+    public void isFinished() {
+        DefaultRunNotifier notifier = new DefaultRunNotifier();
+        final String id = "id";
+        assertFalse(notifier.isFinished(id));
+        notifier.finished(id);
+        assertTrue(notifier.isFinished(id));
+    }
+}
