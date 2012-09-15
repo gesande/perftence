c3ee3241738a3960502d6aad3919b4e617bc7981
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2012-09-15 15:48:16 +0000

    introduced distribution package script, useful for publishing stuff to sourceforge

--- trunk/build/distribution/package.sh
+++ trunk/build/distribution/package.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-set -eu
+set -e
 
-gradle perftence:dist perftence:sourcesJar responsecode-summaryappender:sourcesJar responsecode-summaryappender:dist perftence-junit-utils:sourcesJar perftence-junit-utils:dist
 
+tar-file() {
+  local REV=$(svnversion)
+  local ARTIFACT=perftence-distribution-$REV.tar
+  echo "ARTIFACT =$ARTIFACT"
+  tar -cvf $ARTIFACT **/build/distributions/*.zip **/build/libs/*sources.jar
+}
+
+build-packages() {
+  gradle clean perftence:dist perftence:sourcesJar responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar
+}
+
+build-packages
+tar-file
