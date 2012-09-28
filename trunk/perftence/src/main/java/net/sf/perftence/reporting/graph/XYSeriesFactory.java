824d2eda918a290a21a1365f2b8c76a36fb0cc27
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2012-09-28 12:01:57 +0000

    introducing XYSeriesFactory

---
+++ trunk/perftence/src/main/java/net/sf/perftence/reporting/graph/XYSeriesFactory.java
@@ -0,0 +1,11 @@
+package net.sf.perftence.reporting.graph;
+
+import org.jfree.data.xy.XYSeries;
+
+@SuppressWarnings("static-method")
+public class XYSeriesFactory {
+
+    public XYSeries newXYSeries(final String legend) {
+        return new XYSeries(legend, false);
+    }
+}
