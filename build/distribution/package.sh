61be11ba4e1e238cf1c5ea673ebb8f6c00b806c0
gsande <geir.sande@luukku.com>
2013-01-23 11:32:39 +0000

    runs now acceptance-tests when making a distribution package

--- build/distribution/package.sh
+++ build/distribution/package.sh
@@ -25,7 +25,7 @@ clean-distribution() {
 }
 
 build-packages() {
-  gradle clean perftence:test perftence:dist perftence:sourcesJar responsecode-summaryappender:test responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar
+  gradle clean perftence:test responsecode-summaryappender:test acceptance-tests:test perftence:dist perftence:sourcesJar  responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar
 }
 
 build-packages
