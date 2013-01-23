13cb406cac1f3effd90ca2baa8e05dfe11fc7b09
gsande <geir.sande@luukku.com>
2013-01-23 12:09:51 +0000

    backlogging

--- backlog/show.sh
+++ backlog/show.sh
@@ -30,7 +30,7 @@ task-done "enabled last second throughput graph writing also for agent based tes
 task-done "agent based tests: category specific invocation reporters also respect noInvocationGraph() setting" $(tag feature)
 task-done "agent based tests: optimize graph, no same graphs from overall to  category specific reports" $(tag feature)
 task-done "agent based tests: Ability to turn off 'threads running current tasks' and task schedule differencies' measurements" $(tag feature)
-task-done "acceptance-tests are also run when making a distribution package"
+task-done "acceptance-tests are also run when making a distribution package" $(tag feature)
 
 in-progress-title
 
