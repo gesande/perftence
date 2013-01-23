99876fa15098b88caca880ec86d41b2b8b41d168
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-01-23 12:36:47 +0000

    backlogging

--- trunk/backlog/show.sh
+++ trunk/backlog/show.sh
@@ -36,23 +36,24 @@ in-progress-title
 
 waiting-title
 
+waiting "use emma to figure out where to put some more unit tests" $(tag code-quality)
 waiting "provide means to create test results in an intermediate format" $(tag feature)
 intended-comment "two-phased construction -> first tests -> produce results to an intermediate format"
 waiting "provide means to create test report from an intermediate format" $(tag feature)
 intended-comment "second phase -> create test reports from intermediate format"
+waiting "provide success rate percentage for intermediate statistics" $(tag feature)
+waiting "last second throughput graph for agent based tests" $(tag feature)
+waiting "provide tools to create ResponseCodesPerSecond graph, see http://code.google.com/p/jmeter-plugins/wiki/ResponseCodesPerSecond for example" $(tag feature)
+waiting "split fluent and agent stuff into separate projects" $(tag refactoring)
+intended-comment "may reflect to need of: separate 'perftence' and 'report' stuff" 
 waiting "new line-reader project" $(tag development)
 waiting "provide line-reader project in distribution package" $(tag development)
-waiting "provide success rate percentage for intermediate statistics" $(tag feature)
-waiting "provide tools to create ResponseCodesPerSecond graph, see http://code.google.com/p/jmeter-plugins/wiki/ResponseCodesPerSecond for example" $(tag feature)
-waiting "last second throughput graph for agent based tests" $(tag feature)
 waiting "provide means to define also Throwable/Error with allow() mechanism" $(tag feature)
 waiting "provide means to tell the Executable  also Throwable/Error with allow() mechanism" $(tag feature)
 waiting "provide template pom.xml for developers" $(tag development-support)
 waiting "upload test example distribution packages to project files (i.e. to source forge)" $(tag development-support)
 waiting "move build.gradle & settings.gradle under build project" $(tag build)
 waiting "make shell script library for build tasks" $(tag build)
-waiting "split fluent and agent stuff into separate projects" $(tag refactoring)
-intended-comment "may reflect to need of: separate 'perftence' and 'report' stuff" 
 waiting "write unit tests for summary fields" $(tag tests)
 waiting "one intermediate summary statistics appender" $(tag refactoring)
 intended-comment "define each summary item as a separate 'plugin' which is registered to one implementation --> one summary builder for agent and executable -based tests, each define their own summary 'plugin'"
@@ -65,3 +66,4 @@ intended-comment "public interface DatasetAdapter GRAPHDATA to DatasetAdapter GR
 waiting "ability to define the TPS is used defining it before running the test" $(tag feature)
 intended-comment "e.g. running at 500 TPS max when the test is running"
 waiting "simplified thread run engine for finding out threading issues" $(tag feature)
+intended-comment "introduce new project called perftence-thread-engine"
