1176c2a5a499d48fcb898489efaf80e59a3233f3
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-01-24 10:59:20 +0000

    more unit tests

---
+++ trunk/perftence/src/test/java/net/sf/perftence/PerformanceRequirementValidatorTest.java
@@ -0,0 +1,109 @@
+package net.sf.perftence;
+
+import org.junit.Test;
+
+public class PerformanceRequirementValidatorTest {
+
+    @SuppressWarnings("static-method")
+    @Test(expected = PerfTestFailure.class)
+    public void maxLatency() {
+        final int max = 50;
+        PerformanceRequirements requirements = new PerformanceRequirements() {
+
+            @Override
+            public long totalTime() {
+                return 1000;
+            }
+
+            @Override
+            public int throughput() {
+                return 100;
+            }
+
+            @Override
+            public PercentileRequirement[] percentileRequirements() {
+                return new PercentileRequirement[0];
+            }
+
+            @Override
+            public int median() {
+                return 100;
+            }
+
+            @Override
+            public int max() {
+                return max;
+            }
+
+            @Override
+            public int average() {
+                return 100;
+            }
+        };
+        StatisticsProvider statisticsProvider = new StatisticsProvider() {
+
+            @Override
+            public double throughput() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+
+            @Override
+            public long sampleCount() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+
+            @Override
+            public long percentileLatency(int percentile) {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+
+            @Override
+            public long minLatency() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+
+            @Override
+            public long median() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+
+            @Override
+            public long maxLatency() {
+                // TODO Auto-generated method stub
+                return 100;
+            }
+
+            @Override
+            public boolean hasSamples() {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public long duration() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+
+            @Override
+            public double averageLatency() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+        };
+        newValidator(requirements, statisticsProvider).checkRuntimeLatency("id",
+                500);
+    }
+
+    private static PerformanceRequirementValidator newValidator(
+            PerformanceRequirements requirements,
+            StatisticsProvider statisticsProvider) {
+        return new PerformanceRequirementValidator(requirements,
+                statisticsProvider, new PerfTestFailureFactory());
+    }
+}
