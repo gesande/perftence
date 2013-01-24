234125f83bd870b0678ff51b53c7f4f035a0daee
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-01-24 13:57:48 +0000

    removed experimental code

--- trunk/perftence/src/main/java/net/sf/perftence/reporting/FrequencyStorageFactory.java
+++ trunk/perftence/src/main/java/net/sf/perftence/reporting/FrequencyStorageFactory.java
@@ -6,8 +6,6 @@ import net.sf.perftence.reporting.graph.DatasetAdapterFactory;
 import net.sf.perftence.reporting.graph.ImageData;
 import net.sf.perftence.reporting.graph.LineChartGraphData;
 
-import org.apache.commons.collections.Bag;
-
 public final class FrequencyStorageFactory {
 
     private FrequencyStorageFactory() {
@@ -54,29 +52,5 @@ public final class FrequencyStorageFactory {
     private static DatasetAdapter<LineChartGraphData> adapterForLinechart(
             final String legendTitle) {
         return DatasetAdapterFactory.adapterForLineChart(legendTitle);
-    }
-
-    public static FrequencyStorage newFrequencyStorage(final Bag values) {
-        return new FrequencyStorage() {
-            @Override
-            public ImageData imageData() {
-                final String legendTitle = legendTitle();
-                final ImageData imageData = newImageData(legendTitle);
-                long range = 0;
-                for (final Object value : values.uniqueSet()) {
-                    final int count = values.getCount(value);
-                    if (count > range) {
-                        range = count;
-                    }
-                    imageData.add((Number) value, count);
-                }
-                return imageData.range(range);
-            }
-
-            @Override
-            public boolean hasSamples() {
-                return !values.isEmpty();
-            }
-        };
     }
 }
