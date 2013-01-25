816a5b7b8fc67f0e5e5c6dd08543c5c1a0f7221d
gsande <geir.sande@luukku.com>
2013-01-25 08:00:47 +0000

    modified some limits for the requirement tests

--- perftence/src/test/java/net/sf/perftence/fluent/FluentPerformanceTestTest.java
+++ perftence/src/test/java/net/sf/perftence/fluent/FluentPerformanceTestTest.java
@@ -100,7 +100,7 @@ public class FluentPerformanceTestTest extends AbstractMultiThreadedTest {
         final TestBuilder builder = new FluentPerformanceTest(
                 new PerfTestFailedNotifier()).test(id());
         builder.setup(setup().threads(1).invocations(5).build())
-                .requirements(requirements().percentile95(100).max(102).build())
+                .requirements(requirements().percentile95(102).max(102).build())
                 .executable(new Executable() {
                     @Override
                     public void execute() throws Exception {
