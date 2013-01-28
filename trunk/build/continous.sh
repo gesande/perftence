95f810e72d298ca3ceeb3a6ae7f6eae32118acd1
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-01-28 13:18:29 +0000

    added total build time to the continous build

--- trunk/build/continous.sh
+++ trunk/build/continous.sh
@@ -5,9 +5,11 @@ continous-build() {
   local PROJECT=$1
   gradle --info $PROJECT:clean $PROJECT:test $PROJECT:copy-pmd-settings $PROJECT:findbugsMain $PROJECT:pmdMain $PROJECT:jdependMain
 }
-
+start_time=`date +%s`
 continous-build perftence
 continous-build responsecode-summaryappender
 continous-build perftence-junit-utils
 continous-build perftence-classhelper
-
+end_time=`date +%s`
+echo
+echo TOTAL BUILD TIME: `expr $end_time - $start_time` s.
