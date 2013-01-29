f2fb98475c9ea780be0c55a1f3d1cbb4a9668039
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-01-29 08:50:46 +0000

    refactoring SchedulingServiceFactory <- knows how to create ScheduledThreadPoolExecutors and passes that to the SchedulingService

--- trunk/perftence/src/main/java/net/sf/perftence/agents/SchedulingServiceFactory.java
+++ trunk/perftence/src/main/java/net/sf/perftence/agents/SchedulingServiceFactory.java
@@ -1,13 +1,15 @@
 package net.sf.perftence.agents;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
 final class SchedulingServiceFactory {
 
     @SuppressWarnings("static-method")
-    public TestTaskSchedulingService newBasedOnJavaConcurrentStuff(
+    public TestTaskSchedulingService newSchedulingService(
             final RunnableAdapter adapter, final int workers,
             final ScheduledTasks scheduledTasks) {
         return new SchedulingServiceBasedOnJavaConcurrentStuff(adapter,
-                workers, scheduledTasks);
+                scheduledTasks, new ScheduledThreadPoolExecutor(workers));
     }
 
 }
