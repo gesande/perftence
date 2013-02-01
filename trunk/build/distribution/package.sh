9627ba49b65cdc3ee7705527fa6e38e9a67b94b2
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-02-01 12:41:36 +0000

    gradle build for perftence-bag

--- trunk/build/distribution/package.sh
+++ trunk/build/distribution/package.sh
@@ -24,8 +24,8 @@ clean-distribution() {
 }
 
 build-packages() {
-  gradle clean perftence:test responsecode-summaryappender:test acceptance-tests:test perftence-junit-utils:test perftence-classhelper:test perftence-linereader:test
-  gradle clean perftence:dist perftence:sourcesJar responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar perftence-classhelper:dist perftence-classhelper:sourcesJar perftence-linereader:dist perftence-linereader:sourcesJar
+  gradle clean perftence:test responsecode-summaryappender:test perftence-junit-utils:test perftence-classhelper:test perftence-linereader:test acceptance-tests:test
+  gradle clean perftence:dist perftence:sourcesJar responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar perftence-classhelper:dist perftence-classhelper:sourcesJar perftence-linereader:dist perftence-linereader:sourcesJar perftence-bag:dist perftence-bag:sourcesJar
 }
 
 start_time=`date +%s`
