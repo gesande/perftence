69f5fedd34a56280258380c3bb7608eef96d5968
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-02-04 11:22:49 +0000

    added perftence-junit to the distribution package

--- trunk/build/distribution/package.sh
+++ trunk/build/distribution/package.sh
@@ -24,8 +24,8 @@ clean-distribution() {
 }
 
 build-packages() {
-  gradle clean perftence-bag:test perftence:test responsecode-summaryappender:test perftence-junit-utils:test perftence-classhelper:test perftence-linereader:test acceptance-tests:test
-  gradle clean perftence:dist perftence:sourcesJar responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar perftence-classhelper:dist perftence-classhelper:sourcesJar perftence-linereader:dist perftence-linereader:sourcesJar perftence-bag:dist perftence-bag:sourcesJar
+  gradle clean perftence-bag:test perftence:test responsecode-summaryappender:test perftence-junit-utils:test perftence-classhelper:test perftence-linereader:test perftence-junit:test acceptance-tests:test
+  gradle clean perftence:dist perftence:sourcesJar responsecode-summaryappender:dist responsecode-summaryappender:sourcesJar perftence-junit-utils:dist perftence-junit-utils:sourcesJar perftence-classhelper:dist perftence-classhelper:sourcesJar perftence-linereader:dist perftence-linereader:sourcesJar perftence-bag:dist perftence-bag:sourcesJar perftence-junit:dist perftence-junit:sourcesJar
 }
 
 start_time=`date +%s`
