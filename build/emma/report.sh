8026cbdd54ba7f74a53007c32418a23dd1ac64e6
gsande <geir.sande@luukku.com>
2013-02-11 09:46:01 +0000

    overall emma report included to the continous build

--- build/emma/report.sh
+++ build/emma/report.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -eu
-java -cp tools/emma-2.1.5320-lib/emma.jar emma report -r html -in $1
+
+generate-emma-html-report() {
+  echo "Creating overall emma coverage"
+  local FILES=$(find -name *.emma)
+  local IN=$(for file in $FILES; do echo -n "-in $file ";done)
+  java -cp tools/emma-2.1.5320-lib/emma.jar emma report -r html $IN -Dreport.out.file=tmp/emma/coverage.html
+  echo "Overall emma coverage report: from file://$(realpath tmp/emma/coverage.html)"
+}
+
+generate-emma-html-report
