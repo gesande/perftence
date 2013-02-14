d5937b6bc84c7cc9f33eae86fc55fd8214a28c46
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-02-14 09:41:40 +0000

    added sourcepath to the overall emma report

--- trunk/build/emma/report.sh
+++ trunk/build/emma/report.sh
@@ -5,7 +5,8 @@ generate-emma-html-report() {
   echo "Creating overall emma coverage report..."
   local FILES=$(find -name *.emma)
   local IN=$(for file in $FILES; do echo -n "-in $file ";done)
-  java -cp tools/emma-2.1.5320-lib/emma.jar emma report -r html $IN -Dreport.out.file=tmp/emma/coverage.html
+  local SOURCEPATH=$(for source in $(ls **/src/main/java); do echo -n "-sourcepath $source ";done)
+  java -cp tools/emma-2.1.5320-lib/emma.jar emma report -report html $SOURCEPATH  $IN -Dreport.out.file=tmp/emma/coverage.html
   echo "Overall emma coverage report: file://$(realpath tmp/emma/coverage.html)"
 }
 
