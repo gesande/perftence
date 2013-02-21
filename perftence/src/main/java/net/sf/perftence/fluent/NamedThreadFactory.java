c8ee05c73af7b896258b3c1dd4634824ab827225
gsande <geir.sande@luukku.com>
2013-02-21 13:45:54 +0000

    introducing NamedThreadFactory

---
+++ perftence/src/main/java/net/sf/perftence/fluent/NamedThreadFactory.java
@@ -0,0 +1,32 @@
+package net.sf.perftence.fluent;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public final class NamedThreadFactory implements ThreadFactory {
+
+    private final AtomicInteger id = new AtomicInteger(0);
+    private final String namePrefix;
+
+    private NamedThreadFactory(final String namePrefix) {
+        this.namePrefix = namePrefix;
+    }
+
+    @Override
+    public Thread newThread(final Runnable runnable) {
+        return new Thread(runnable, prefix() + id().getAndIncrement());
+    }
+
+    private String prefix() {
+        return this.namePrefix;
+    }
+
+    private AtomicInteger id() {
+        return this.id;
+    }
+
+    public static ThreadFactory forNamePrefix(final String namePrefix) {
+        return new NamedThreadFactory(namePrefix);
+    }
+
+}
