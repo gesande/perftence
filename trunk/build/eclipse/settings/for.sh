e8e6955ece994df8e348e87f082e4c1984957e11
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-02-21 10:53:34 +0000

    separating build related stuff into separate gradle scripts

--- trunk/build/eclipse/settings/for.sh
+++ trunk/build/eclipse/settings/for.sh
@@ -4,6 +4,6 @@ set -eu
 settings-for() {
   local PROJECT=$1
   gradle --info :$PROJECT:cleanEclipse :$PROJECT:eclipse
-} 
+}
 
 settings-for $1
--- trunk/build/new/java/project.sh
+++ trunk/build/new/java/project.sh
@@ -2,7 +2,7 @@
 set -eu
 new-java-project() {
   local PROJECT=$1
-  gradle $PROJECT:create-dirs  
+  gradle $PROJECT:create-dirs
   echo "$PROJECT directory structure done."
   svn add $PROJECT
   ./build/svn/ignore.sh $PROJECT
