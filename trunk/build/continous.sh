736a13beacfef87306236ca6fa2da065695713d3
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-02-22 08:20:21 +0000

    moved flesh to the perftence-concurrent module

--- trunk/build/continous.sh
+++ trunk/build/continous.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-gradle clean perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous
+gradle clean perftence-concurrent:continous perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous
 gradle acceptance-tests:test
 build/emma/report.sh
 build/findbugs/report.sh
