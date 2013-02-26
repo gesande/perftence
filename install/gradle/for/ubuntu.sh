06761ef87f85811237347e9ffb1f56467901f4af
gsande <geir.sande@luukku.com>
2013-02-26 07:44:30 +0000

    added gradle 1.3-all package with sources

--- install/gradle/for/ubuntu.sh
+++ install/gradle/for/ubuntu.sh
@@ -3,8 +3,9 @@ set -eu
 HERE=$(dirname "$0")
 
 rm -rf /opt/gradle-1.0-rc-1
+rm -rf /opt/gradle-1.3
 
-unzip -q $HERE/gradle-1.3-bin.zip -d /opt/
+unzip -q $HERE/gradle-1.3-all.zip -d /opt/
 echo "unzip successful"
 rm -rf /opt/gradle
 ln -s /opt/gradle-1.3 /opt/gradle
