e8328fdb2fe5de06c454929469e6ff24a865b3ea
gsande <geir.sande@luukku.com>
2013-02-27 09:46:59 +0000

    distribution package can now be maked with gradle

--- build/distribution/package.sh
+++ build/distribution/package.sh
@@ -3,7 +3,7 @@ set -e
 
 tar-file() {
   local REVISION=$(svnversion)
-  local VERSION=$(gradle perftence:show-version | grep -A1 :perftence:show-version | tail -1)
+  local VERSION=$(gradle perftence:showVersion | grep -A1 :perftence:showVersion | tail -1)
   local ARTIFACT_VERSION="$VERSION-R$REVISION"
   local ARTIFACT_FINAL="tmp/artifact-$ARTIFACT_VERSION/perftence-distribution-$ARTIFACT_VERSION.tar"
 
@@ -15,7 +15,7 @@ tar-file() {
   tar -cvf $ARTIFACT_FINAL distribution
 
   mv distribution tmp/distribution-$ARTIFACT_VERSION
-  
+
   echo "Distribution package can be found from $(in-green file://$(realpath $ARTIFACT_FINAL))"
   echo "Exploded path for the distribution is here: $(in-green file://$(realpath tmp/distribution-$ARTIFACT_VERSION))"
 }
@@ -36,8 +36,8 @@ prepare-distribution() {
 }
 
 build-packages() {
-  gradle clean perftence-concurrent:continous perftence-fileutil:continous perftence-bag:continous perftence:continous responsecode-summaryappender:continous perftence-api:continous perftence-junit-utils:continous perftence-classhelper:continous perftence-linereader:continous perftence-junit:continous acceptance-tests:test
-  gradle clean perftence-concurrent:release perftence-bag:release perftence-api:release perftence-junit-utils:release perftence-classhelper:release perftence-linereader:release perftence-fileutil:release perftence:release responsecode-summaryappender:release perftence-junit:release
+#  gradle --stacktrace clean perftence-concurrent:continous perftence-fileutil:continous perftence-bag:continous perftence:continous responsecode-summaryappender:continous perftence-api:continous perftence-junit-utils:continous perftence-classhelper:continous perftence-linereader:continous perftence-junit:continous acceptance-tests:test
+  gradle --stacktrace clean perftence-concurrent:release perftence-bag:release perftence-api:release perftence-junit-utils:release perftence-classhelper:release perftence-linereader:release perftence-fileutil:release perftence:release responsecode-summaryappender:release perftence-junit:release
 }
 
 build-packages
