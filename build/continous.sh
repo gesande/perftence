6f827080c9a1f748b74b0e5d00ea9eecbde867fd
gsande <geir.sande@luukku.com>
2013-02-27 11:20:23 +0000

    separate build.gradle files / project, also some build script refactoring

--- build/continous.sh
+++ build/continous.sh
@@ -2,8 +2,7 @@
 set -eu
 
 gradle clean
-gradle --stacktrace perftence-concurrent:continous perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous acceptance-tests:test
-gradle aggregateTestReport
+gradle --stacktrace perftence-concurrent:continous perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous acceptance-tests:test aggregateTestReport
 build/emma/report.sh
 build/findbugs/report.sh
 build/jdepend/report.sh
