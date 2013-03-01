8ec2e100e6c8f3a4664ca8cc4c06ac7f82d3be89
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-01 18:51:24 +0000

    added aggregated reports to the continous build

--- trunk/build/continous.sh
+++ trunk/build/continous.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -eu
 
-gradle clean
-gradle --stacktrace perftence-concurrent:continous perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous acceptance-tests:test aggregateTestReport aggregateJDependReport
-build/emma/report.sh
-build/findbugs/report.sh
-
+gradle --stacktrace clean perftence-concurrent:continous perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous acceptance-tests:test aggregateTestReport aggregateJDependReport aggregateEmmaReport aggregateFindbugsReport
