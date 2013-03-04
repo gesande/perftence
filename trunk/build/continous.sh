e5eb2d619af27e19461198cbd428e466cf07e1ad
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-04 08:10:35 +0000

    moved continous and dist package stuff into build.gradle instead of shell scripts

--- trunk/build/continous.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eu
-
-gradle --stacktrace clean perftence-concurrent:continous perftence-bag:continous perftence-linereader:continous perftence-fileutil:continous perftence-junit-utils:continous perftence-classhelper:continous perftence:continous perftence-api:continous responsecode-summaryappender:continous perftence-junit:continous acceptance-tests:test aggregateTestReport aggregateJDependReport aggregateEmmaReport aggregateFindbugsReport
