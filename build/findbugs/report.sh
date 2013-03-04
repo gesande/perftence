1f94e4e98f0718291c711225c4d4349298b557b4
gsande <geir.sande@luukku.com>
2013-03-04 10:27:33 +0000

    cleaned up some shell scripts

--- build/findbugs/report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-mkdir -p tmp/findbugs
-echo "Creating overall findbugs report..."
-tools/findbugs-2.0.2/bin/findbugs -javahome $JAVA_HOME/jre -textui -html -output tmp/findbugs/index.html -sourcepath **/src/main/java -onlyAnalyze net.sf.perftence.- -auxclasspath **/lib/*.jar $JAVA_HOME/jre/lib/*.jar tools/ext-libs-for-findbugs/*.jar **/build/libs/*.jar
-echo "Overall findbugs report: file://$(realpath tmp/findbugs/index.html)"
-
