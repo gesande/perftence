4160df8fc20ed39712b089267a7be99fd97acc1f
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-06 13:45:52 +0000

    gradle svnRevision,svnStatus

--- trunk/build/svn/status.sh
+++ trunk/build/svn/status.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 set -eu
-
-show-svn-status() {
-  local ST=$(svn st)
-  clear
-  if [ "$ST" = "" ]; then
-    echo "No changes to be committed."
-  else
-    svn st
-  fi
-}
-
-show-svn-status
+clear && svn st && echo "Revision: $(svnversion)"
