62cf4070bb028354f47bfab8692124dd5c444a2e
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-08 11:13:38 +0000

    javadocs, also made api change to ThreadEngineApi

--- trunk/perftence-concurrent/src/main/java/net/sf/perftence/concurrent/ThreadEngineApi.java
+++ trunk/perftence-concurrent/src/main/java/net/sf/perftence/concurrent/ThreadEngineApi.java
@@ -1,25 +1,38 @@
 package net.sf.perftence.concurrent;
 
+/**
+ * Easy API for running something being or extending Runnable in threads.
+ * 
+ */
 public final class ThreadEngineApi<RUNNABLE extends Runnable> {
 
     private RUNNABLE[] runnables;
     private ThreadEngine threadEngine;
 
-    public ThreadEngineApi<RUNNABLE> with(final String namePrefix) {
+    /**
+     * When a thread is created this prefix is used as a part for its name.
+     */
+    public ThreadEngineApi<RUNNABLE> threadNamePrefix(final String prefix) {
         this.threadEngine = new ThreadEngine(
-                NamedThreadFactory.forNamePrefix(namePrefix));
+                NamedThreadFactory.forNamePrefix(prefix));
         return this;
     }
 
-    public ThreadEngineApi<RUNNABLE> with(final RUNNABLE... runnables) {
+    public ThreadEngineApi<RUNNABLE> runnables(final RUNNABLE... runnables) {
         this.runnables = runnables;
         return this;
     }
 
+    /**
+     * This call blocks until all threads are finished.
+     */
     public void run() {
         engine().run(runnables());
     }
 
+    /**
+     * Interrupts threads running the runnables.
+     */
     public void interrupt() {
         engine().interruptThreads();
     }
