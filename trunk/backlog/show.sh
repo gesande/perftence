bba3725601576e8f52d578ef02e4c55adfc22d3a
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-08 08:10:58 +0000

    gradle testCodeDist,testCodeRelease, also moved COPYING file to src/test/resources for those projects that provide just test code

--- trunk/backlog/show.sh
+++ trunk/backlog/show.sh
@@ -72,6 +72,7 @@ task-done "gradle addNewFilesToSvn,listFilesNotAddedToSvn" $(tag build)
 task-done "gradle printChangeLog" $(tag build)
 task-done "gradle exportChangeLog" $(tag build)
 task-done "threadengine-api-example module" $(tag development-support)
+task-done "gradle testCodeDist,testCodeRelease" $(tag development-support)
 
 in-progress-title
 
