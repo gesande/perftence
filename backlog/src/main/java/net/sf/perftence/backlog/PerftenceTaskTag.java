86de7f9816434ed1dabaa5d53cab6b1fcf0a5e60
gsande <geir.sande@luukku.com>
2013-03-13 12:34:44 +0000

    simple-backlog module

--- backlog/src/main/java/net/sf/perftence/backlog/PerftenceTaskTag.java
+++ backlog/src/main/java/net/sf/perftence/backlog/PerftenceTaskTag.java
@@ -3,5 +3,5 @@ package net.sf.perftence.backlog;
 import net.sf.simplebacklog.Tag;
 
 public enum PerftenceTaskTag implements Tag {
-    waiting, inProgress, developmentSupport, development, ide, codeQuality, deployment, feature, infrastructure, refactoring, build
+    waiting, inProgress, developmentSupport, development, ide, codeQuality, deployment, feature, infrastructure, refactoring, build, backlog
 }
