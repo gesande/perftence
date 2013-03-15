13c3f6e62551a98adf14c85b0138686ce011d017
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-15 13:44:58 +0000

    added buildSrc

---
+++ trunk/buildSrc/src/main/groovy/net/sf/perftence/build/BashExec.groovy
@@ -0,0 +1,12 @@
+package net.sf.perftence.build
+
+import org.gradle.api.tasks.Exec
+
+
+public class BashExec extends Exec {
+
+    BashExec() {
+        executable="bash"
+        args("-c")
+    }
+}
