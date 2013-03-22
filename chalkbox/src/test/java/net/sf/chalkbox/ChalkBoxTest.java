4276da2952992ad99445e3393c1ef643d8e677e2
gsande <geir.sande@luukku.com>
2013-03-22 07:06:03 +0000

    introduced all known chalks, also added tests for them

--- chalkbox/src/test/java/net/sf/chalkbox/ChalkBoxTest.java
+++ chalkbox/src/test/java/net/sf/chalkbox/ChalkBoxTest.java
@@ -1,28 +1,59 @@
 package net.sf.chalkbox;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
-import net.sf.chalkbox.ChalkBox;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChalkBoxTest {
+    private ChalkBox chalks;
 
-    @SuppressWarnings("static-method")
+    @Before
+    public void before() {
+        this.chalks = new ChalkBox();
+    }
+
     @Test
-    public void test() throws FileNotFoundException, IOException {
-        StringBuilder sb = new StringBuilder();
-        ChalkBox box = new ChalkBox();
-        sb.append(box.green().write("green")).append("\n");
-        sb.append(box.red().write("red")).append("\n");
-        sb.append(box.yellow().write("yellow")).append("\n");
-        File file = new File("target");
-        file.mkdirs();
-        new FileOutputStream(new File(file, "checkthisout")).write(sb
-                .toString().getBytes());
-        System.out.println(sb.toString());
+    public void black() {
+        assertEquals("[30mblack[m", chalks().black().write("black"));
+    }
+
+    @Test
+    public void red() {
+        assertEquals("[31mred[m", chalks().red().write("red"));
+    }
+
+    @Test
+    public void green() {
+        assertEquals("[32mgreen[m", chalks().green().write("green"));
+    }
+
+    @Test
+    public void yellow() {
+        assertEquals("[33myellow[m", chalks().yellow().write("yellow"));
+    }
+
+    @Test
+    public void blue() {
+        assertEquals("[34mblue[m", chalks().blue().write("blue"));
+    }
+
+    @Test
+    public void magenta() {
+        assertEquals("[35mmagenta[m", chalks().magenta().write("magenta"));
+    }
+
+    @Test
+    public void cyan() {
+        assertEquals("[36mcyan[m", chalks().cyan().write("cyan"));
+    }
+
+    @Test
+    public void white() {
+        assertEquals("[37mwhite[m", chalks().white().write("white"));
+    }
+
+    private ChalkBox chalks() {
+        return this.chalks;
     }
 }
