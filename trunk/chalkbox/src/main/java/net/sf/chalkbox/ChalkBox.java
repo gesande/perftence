0d5b77da3065f7fbc9bd315bae71fd54c8bd38ee
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2013-03-22 07:06:03 +0000

    introduced all known chalks, also added tests for them

--- trunk/chalkbox/src/main/java/net/sf/chalkbox/ChalkBox.java
+++ trunk/chalkbox/src/main/java/net/sf/chalkbox/ChalkBox.java
@@ -1,8 +1,9 @@
 package net.sf.chalkbox;
 
+@SuppressWarnings("static-method")
 public final class ChalkBox {
 
-    private static final String PREFIX = "\u001b["; // NOI18N
+    private static final String PREFIX = "\u001b[";
     private static final String SUFFIX = "m";
     private static final String END = PREFIX + SUFFIX;
 
@@ -14,23 +15,42 @@ public final class ChalkBox {
         }
     }
 
-    @SuppressWarnings("static-method")
-    public Chalk green() {
-        return Chalks.Green;
+    public Chalk black() {
+        return Chalks.Black;
     }
 
-    @SuppressWarnings("static-method")
     public Chalk red() {
         return Chalks.Red;
     }
 
-    @SuppressWarnings("static-method")
+    public Chalk green() {
+        return Chalks.Green;
+    }
+
     public Chalk yellow() {
         return Chalks.Yellow;
     }
 
+    public Chalk blue() {
+        return Chalks.Blue;
+    }
+
+    public Chalk magenta() {
+        return Chalks.Magenta;
+    }
+
+    public Chalk cyan() {
+        return Chalks.Cyan;
+    }
+
+    public Chalk white() {
+        return Chalks.White;
+    }
+
     private enum Chalks implements Chalk {
-        Green(AnsiColor.Green), Red(AnsiColor.Red), Yellow(AnsiColor.Yellow);
+        Black(AnsiColor.Black), Red(AnsiColor.Red), Green(AnsiColor.Green), Yellow(
+                AnsiColor.Yellow), Blue(AnsiColor.Blue), Magenta(
+                AnsiColor.Magenta), Cyan(AnsiColor.Cyan), White(AnsiColor.White);
 
         private AnsiColor color;
 
