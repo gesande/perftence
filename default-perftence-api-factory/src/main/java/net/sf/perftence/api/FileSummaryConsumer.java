a00bf87446958ce7cdb36bc1fcaf6d45b1030225
gsande <geir.sande@luukku.com>
2017-04-05 13:06:43 +0000

    ticket#2 : summary in csv format

--- default-perftence-api-factory/src/main/java/net/sf/perftence/api/FileSummaryConsumer.java
+++ default-perftence-api-factory/src/main/java/net/sf/perftence/api/FileSummaryConsumer.java
@@ -1,5 +1,8 @@
 package net.sf.perftence.api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import net.sf.perftence.common.SummaryConsumer;
 import net.sf.völundr.fileio.AppendToFileFailed;
 import net.sf.völundr.fileio.FileUtil;
@@ -15,10 +18,34 @@ public final class FileSummaryConsumer implements SummaryConsumer {
 	@Override
 	public void consumeSummary(String summaryId, String summary) {
 		try {
-			FileUtil.appendToFile(path + "/" + summaryId,
-					ToBytes.withDefaultCharset().convert(summary));
-		} catch (AppendToFileFailed e) {
+			FileUtil.appendToFile(path + "/" + summaryId, ToBytes
+					.withDefaultCharset().convert(convertToCsv(summary)));
+		} catch (AppendToFileFailed | IOException e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	private static String convertToCsv(String summary) throws IOException {
+		StringReader reader = new StringReader(summary);
+		BufferedReader br = new BufferedReader(reader);
+		try {
+			StringBuilder cols = new StringBuilder();
+			StringBuilder rows = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(":");
+				String field = values[0].replaceAll(" ", "_");
+				String value = values[1].trim();
+				cols.append(field + ",");
+				rows.append(value + ",");
+			}
+			String string = cols.toString();
+			String string2 = rows.toString();
+			return string.substring(0, string.length() - 1) + "\n"
+					+ string2.substring(0, string2.length() - 1);
+		} finally {
+			br.close();
+			reader.close();
 		}
 	}
 }
