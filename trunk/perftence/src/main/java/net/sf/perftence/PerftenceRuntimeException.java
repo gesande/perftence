a55b9151c13ba3a3837a23dc5c3dbeef520ab73b
gsande <gsande@210d9044-cc67-46c9-8bd9-4ec250bfd5e9>
2017-08-01 11:13:13 +0000

    PerftenceRuntimeException

---
+++ trunk/perftence/src/main/java/net/sf/perftence/PerftenceRuntimeException.java
@@ -0,0 +1,17 @@
+package net.sf.perftence;
+
+public final class PerftenceRuntimeException extends RuntimeException {
+
+    public PerftenceRuntimeException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public PerftenceRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public PerftenceRuntimeException(String msg) {
+        super(msg);
+    }
+
+}
